6bcc13430635193ce3c816cdc3be36479b4b72bb
Vansh Kushwaha <kushwahavansh90@gmail.com>
2026-05-03 14:33:23 +0530

    fixed post page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import { useState } from "react";
-import ItemCard from "./components/ItemCard";
-import ItemForm from "./components/ItemForm";
 import Home from './pages/Home'
 import Browse from './pages/Browse'
 import Details from './pages/Details'
@@ -11,24 +9,11 @@ import Post from './pages/Post'
 
 function App() {
 
-  const [items, setItems] = useState([]);
-
-  const handleAdd = (newItem) => {
-    setItems([newItem, ...items]);
-  };
+  
 
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <div className="p-4">
-        <ItemForm onAdd={handleAdd} />
-
-        <div className="grid grid-cols-3 gap-4 mt-4">
-          {items.map((item) => (
-            <ItemCard key={item.id} item={item} />
-          ))}
-        </div>
-      </div>
       <main className="max-w-2xl mx-auto p-4">
         <Routes>
           <Route path="/" element={<Home />} />
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,29 @@
+import {useState} from "react"
+import ItemCard from "../components/ItemCard";
+import ItemForm from "../components/ItemForm";
+
 export default function Post() {
+
+	const [items, setItems] = useState([]);
+
+	const handleAdd = (newItem) => {
+		setItems([newItem, ...items]);
+	};
+
 	return (
 		<div className="prose">
 			<h1>Post</h1>
 			<p>Create a new post here.</p>
+			<div className="p-4">
+			<ItemForm onAdd={handleAdd} />
+
+			<div className="grid grid-cols-3 gap-4 mt-4">
+			{items.map((item) => (
+				<ItemCard key={item.id} item={item} />
+			))}
+			</div>
+		</div>
+
 		</div>
 	)
 }
